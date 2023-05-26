e8449a45142b2b3603589f6fd87ed2cc404dadd7
BalderramasNathaniel <92334@sistemas.frc.utn.edu.ar>
2023-05-26 08:16:10 -0300

    Initialize first tests

---
+++ tests/app.test.js
@@ -0,0 +1,25 @@
+// allows acces to enviroment variables in proces.env
+require('dotenv').config() 
+
+const request = require("supertest");
+const app = require("../app");
+
+// Levanta el servidor antes de empezar a testear
+beforeAll(()=>{
+  server = app.listen(4020)
+});
+
+// Cierra el servidor despues de testear
+afterAll(()=>{
+  server.close()
+});
+
+
+describe("GET /", () => {
+    it("should return a welcome message", async () => {
+      const res = await request(app).get("/");
+      expect(res.statusCode).toBe(200);
+      expect(res.text.length).toBeGreaterThan(10);
+    });
+  });
+  
