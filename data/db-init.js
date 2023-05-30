3b2666fcfaa83da12d4feee493a90a28f62bab17
BalderramasNathaniel <92334@sistemas.frc.utn.edu.ar>
2023-05-30 16:37:49 -0300

    fixed issues with database dialect from env

--- data/db-init.js
+++ data/db-init.js
@@ -1,12 +1,10 @@
 require('dotenv').config({path: "../.env"});
 const Sequelize = require("sequelize");
-
 function db_init (database, user, password, dialect) {
     const sequelize = new Sequelize(database, user, password, {dialect: dialect});
     return sequelize;
 }
 
-// Por alguna razon cunando uso process.env.DIALECT (que vale "sqlite") me da error :/
-const db = db_init(process.env.DATABASE, process.env.USER, process.env.PASSWORD, "sqlite");
+const db = db_init(process.env.DATABASE, process.env.USER, process.env.PASSWORD, process.env.DIALECT);
 
 module.exports = { db };
