fde26bdbf92e04c8ea7baee7932fb50ae93de08e
Octavio <89866@sistemas.frc.utn.edu.ar>
2023-05-31 19:47:01 -0300

    Commit Vehiculos Etapa 1

--- data/model.Vehiculo.js
+++ data/model.Vehiculo.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require("sequelize");
+const { Vehiculo } = require("./db-link");
 /*
 const {db} = require("./db-link");
 const { Alquiler } = require("./model.Alquiler");
@@ -8,13 +9,100 @@ module.exports = function (db) {
     const Vehiculo = db.define(
         "Vehiculo",
         {
-        IdVehiculo: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
+            IDVehiculo: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
             },
-        }, // fields
-        {} // options
+            Marca: {
+                type: DataTypes.STRING(60),
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        args: true,
+                        msg: "Marca es requerido",
+                    },
+                    len: {
+                        args: [3, 60],
+                        msg: "Marca debe ser tipo carateres, entre 3 y 60 de longitud",
+                    },
+                },
+            },
+            Modelo: {
+                type: DataTypes.STRING(60),
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        args: true,
+                        msg: "Modelo es requerido",
+                    },
+                    len: {
+                        args: [3, 60],
+                        msg: "Modelo debe ser tipo carateres, entre 3 y 60 de longitud",
+                    },
+                },
+            },
+            Patente: {
+                type: DataTypes.STRING(7),
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        args: true,
+                        msg: "La Patente es requerido",
+                    },
+                    len: {
+                        args: [6, 7],
+                        msg: "La Patente debe ser Alfanumerico de 7 simbolos",
+                    },
+                },
+                Unique: {
+                    args: true,
+                    msg: "esta Patente ya existe en la tabla!",
+                },
+            },
+            Valor: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        args: true,
+                        msg: "Valor es requerido",
+                    }
+                }
+            },
+            FechaIngreso: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        args: true,
+                        msg: "Fecha Ingreso es requerido",
+                    }
+                }
+            },
+            Estado: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        args: true,
+                        msg: "Estado es requerido",
+                    }
+                }
+            },
+        },
+        {
+            // pasar a mayusculas
+            hooks: {
+                beforeValidate: function (auto, options) {
+                    if (typeof auto.Marca === "string") {
+                        auto.Marca = auto.Marca.toUpperCase().trim();
+                    }
+                },
+            },
+    
+            timestamps: false,
+        }
     );
-    return Vehiculo;
+    return Vehiculo
 };
