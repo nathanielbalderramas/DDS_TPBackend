336ad054d4288b08331a297a30cd208b1d88a4b6
BalderramasNathaniel <92334@sistemas.frc.utn.edu.ar>
2023-06-01 01:08:48 -0300

    partially implements getAlquileresById

--- controllers/controller.alquiler.js
+++ controllers/controller.alquiler.js
@@ -1,39 +1,92 @@
 const { Alquiler, Vehiculo } = require("../data/db-link")
 
-function makeDate () {
+function randomDate () {
     let year = 2001 + Math.floor(Math.random() * 22);
     let month = 1 + Math.floor(Math.random() * 12);
     let day = 1 + Math.floor(Math.random() * 30);
     if (month == 2 && day > 28) {day = 28}
     return `${year}-${month}-${day}`}
 
-function makeMonto () {
+function randomMonto () {
     return Math.floor(Math.random()*2000 + Math.random()*100000 + 20000) 
 }
 
 const getAlquileres = async (req, res, next) => {
     try {
         const alquileres = await Alquiler.findAll();
-        res.status(200).json(alquileres)
+        if (alquileres.length > 0) {
+            res.status(200).json(alquileres);
+        } else {
+            res.status(404).json({mensaje: "!No encontrado!"})
+        }
     } catch (error) {
         res.status(500).json(error)
     }
 }
 
-const makeAlquileresRandom = async (req, res, next) => {
+const getAlquileresById = async (req, res, next) => {
+    try {
+        const alquileres = await Alquiler.findall({
+            where: {IdAlquiler: req.params.id}
+        });
+        if (alquileres.length > 0) {
+            res.status(200).json(alquileres);
+        } else {
+            res.status(404).json({mensaje: "!No encontrado!"})
+        }
+    } catch (error) {
+            res.status(500).json(error);
+    }
+}
+
+const postAlquileres = async (req, res, next) => {
+    try {
+        const inputParams = {
+            FechaInicio: req.params.FechaInicio,
+            FechaFin: req.params.FechaFin,
+            FechaFinReal: req.params.FechaFinReal || null,
+            Monto: req.params.Monto,
+            IdVehiculo: req.params.IdVehiculo,
+        };
+        const alquiler = await Alquiler.create(inputParams);
+        res.status(200) ;
+    } catch (error) {
+        res.status(500);
+    }
+}
+
+const putAlquileres = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        res.status(500);
+    }
+}
+
+
+const deleteAlquileres = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        res.status(500);
+    }
+}
+
+
+const randomMakeAlquileres = async (req, res, next) => {
     try {
         console.log()
         const alquileres = await Alquiler.bulkCreate([
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
-            {IdVehiculo: 1, FechaInicio: new Date(makeDate()), FechaFin: new Date(makeDate()), Monto: makeMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
+            {IdVehiculo: 1, FechaInicio: new Date(randomDate()), FechaFin: new Date(randomDate()), Monto: randomMonto(), },
         ]);
         res.status(500).json(alquileres)
     } catch (error) {
@@ -52,8 +105,14 @@ const makeUnVehiculoFalso = async (req, res, next) => {
         res.status(500).json(error);
     }
 }
+
+
 module.exports = {
     getAlquileres, 
-    makeAlquileresRandom,
+    getAlquileresById,
+    postAlquileres,
+    putAlquileres,
+    deleteAlquileres,
+    randomMakeAlquileres,
     makeUnVehiculoFalso, // must be removed once Vehiculo is implemented
 }
--- routes/alquileres.js
+++ routes/alquileres.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { getAlquileres, makeAlquileresRandom, makeUnVehiculoFalso } = require("../controllers/controller.alquiler");
+const { 
+    getAlquileres, 
+    getAlquileresById,
+    postAlquileres,
+    putAlquileres,
+    deleteAlquileres,
+    randomMakeAlquileres, 
+    makeUnVehiculoFalso 
+} = require("../controllers/controller.alquiler");
 
-router.get("/randomdata/alquileres", makeAlquileresRandom);
 
 router.get("/alquileres", getAlquileres);
+router.get("/alquileres/:id", getAlquileresById);
+router.post("/alquileres", postAlquileres);
+router.put("/alquileres", putAlquileres);
+router.delete("/alquileres", deleteAlquileres);
 
+router.get("/randomdata/alquileres", randomMakeAlquileres);
 router.get("/randomdata/vehiculoFalso", makeUnVehiculoFalso)
+
 
 module.exports = router;
