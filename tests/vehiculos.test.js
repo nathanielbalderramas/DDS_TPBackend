017fe4809621db0ac4d8b708bed576798e2dba75
Octavio <89866@sistemas.frc.utn.edu.ar>
2023-06-01 21:33:21 -0300

    Octavio test Vehiculos Terminado

--- tests/vehiculos.test.js
+++ tests/vehiculos.test.js
@@ -1,28 +1,35 @@
 const request = require("supertest");
-const app = require("../routes/router.Vehiculos");
+const app = require("../app.js");
+
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return  Math.floor(Math.random()*(max-min+1)+min);
+};
+
+function getFechaActual(){
+    let date = new Date()
+    mes = date.getMonth() + 1;
+    fecha = date.getDate() + "-"+ mes + "-" +date.getFullYear();
+    return fecha;
   }
+
 const VehiculoAlta = {
+    FechaIngreso: getFechaActual(),
   Marca: getRandomArbitrary(1,10),
   Modelo: "Vehiculo " + (( ) => (Math.random() + 1).toString(36).substring(2))(),  // Genera un nombre aleatorio
-  Patente: "AA" + (( ) => (getRandomArbitrary(100,999)).toString(36).substring(3))() + "AA",
+  Patente: "AA" + (getRandomArbitrary(100,999)).toString() + "AA",
   Valor: 100345,
-  FechaIngreso: new Date().toISOString(),
-  Estado: 1,
 };
 const vehiculoModificado = {
-  IdArticulo: 1,
-  Marca: getRandomArbitrary(1,10),
-  Modelo: "Vehiculo " + (( ) => (Math.random() + 1).toString(36).substring(2))(),  // Genera un nombre aleatorio
-  Patente: "AA" + (( ) => (getRandomArbitrary(100,999)).toString(36).substring(3))() + "AA",
+    Modelo: "Vehiculo " + (( ) => (Math.random() + 1).toString(36).substring(2))(),
+    Marca: getRandomArbitrary(1,10),
+  // Genera un nombre aleatorio
+  Patente: "AA" + (getRandomArbitrary(100,999)).toString() + "AA",
   Valor: 100345,
-  FechaIngreso: new Date().toISOString(),
-  Estado: 1,
+  FechaIngreso: getFechaActual(),
 };
 
 
-// test route/articulos GET
+// test route/vehiculos GET
 describe("GET /api/vehiculos", () => {
   it("Deberia devolver todos los Vehiculos", async () => {
     const res = await request(app).get("/api/vehiculos");
@@ -32,13 +39,13 @@ describe("GET /api/vehiculos", () => {
       Items: 
       expect.arrayContaining([
         expect.objectContaining({
-          idVehiculo: expect.any(Number),
+            Estado: expect.any(Number),
+            FechaIngreso: expect.any(String),
+            IdVehiculo: expect.any(Number),
           Marca: expect.any(Number),
-          Modelo: expect.any(Number),
+          Modelo: expect.any(String),
           Patente: expect.any(String),
           Valor: expect.any(Number),
-          FechaAlta: expect.any(String),
-          Estado: expect.any(Number),
         })
       ]),
       RegistrosTotal:  expect.any(Number) 
@@ -47,69 +54,56 @@ describe("GET /api/vehiculos", () => {
   });
 });
 
-// test route/articulos/:id GET
+// test route/vehiculos/:id GET
 describe("GET /api/vehiculos/:id", () => {
   it("Deberia devolver el vehiculo con el id 1", async () => {
     const res = await request(app).get("/api/vehiculos/1");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual(
       expect.objectContaining({
-        idVehiculo: expect.any(Number),
+        Estado: expect.any(Number),
+            FechaIngreso: expect.any(String),
+            IdVehiculo: expect.any(Number),
           Marca: expect.any(Number),
-          Modelo: expect.any(Number),
+          Modelo: expect.any(String),
           Patente: expect.any(String),
           Valor: expect.any(Number),
-          FechaAlta: expect.any(String),
-          Estado: expect.any(Number),
       })
     );
   });
 });
 
-// test route/articulos POST
+// test route/vehiculos POST
 describe("POST /api/vehiculos", () => {
   it("Deberia devolver el articulo que acabo de crear", async () => {
     const res = await request(app).post("/api/vehiculos").send(VehiculoAlta);
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual(
       expect.objectContaining({
-        idVehiculo: expect.any(Number),
-        Marca: expect.any(Number),
-        Modelo: expect.any(Number),
-        Patente: expect.any(String),
-        Valor: expect.any(Number),
-        FechaAlta: expect.any(String),
-        Estado: expect.any(Number),
+            FechaIngreso: expect.any(String),
+            Marca: expect.any(Number),
+          Modelo: expect.any(String),
+          Patente: expect.any(String),
+          Valor: expect.any(Number),
       })
     );
   });
 });
 
-// test route/articulos/:id PUT
-describe("PUT /api/articulos/:id", () => {
+// test route/vehiculos/:id PUT 
+// No se puede modificar el vehiculo dado que hay ventas relacionadas con el vehiculo
+describe("PUT /api/vehiculos/:id", () => {
   it("Deberia devolver el articulo con el id 1 modificado", async () => {
-    const res = await request(app).put("/api/articulos/1").send(vehiculoModificado);
+    const res = await request(app).put("/api/vehiculos/1").send(vehiculoModificado);
     expect(res.statusCode).toEqual(200);
   });
 });
 
-// test route/articulos/:id DELETE
+// test route/vehiculos/:id DELETE
 describe("DELETE /api/vehiculos/:id", () => {
   it("Deberia devolver el vehiculos con el id 1 borrado", async () => {
     const res = await request(app).delete("/api/vehiculos/1");
     expect(res.statusCode).toEqual(200);
    // baja logica, no se borra realmente
-     expect(res.body).toEqual(
-       expect.objectContaining({
-        idVehiculo: expect.any(Number),
-        Marca: expect.any(Number),
-        Modelo: expect.any(Number),
-        Patente: expect.any(String),
-        Valor: expect.any(Number),
-        FechaAlta: expect.any(String),
-        Estado: expect.any(Number),
-       })
-    );
-
   });
 });
