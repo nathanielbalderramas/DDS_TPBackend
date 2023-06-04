d1e4f41d89bffad612b8328100373f71dbead2ce
Octavio Escudero <octavioescudero448@gmail.com>
2023-06-04 19:53:01 -0300

    Test completados Vehiculos

--- tests/vehiculos.test.js
+++ tests/vehiculos.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app.js");
+const app = require("../app");
 
 function getRandomArbitrary(min, max) {
     return  Math.floor(Math.random()*(max-min+1)+min);
